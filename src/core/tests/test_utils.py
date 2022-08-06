c1dfcdfafeceeccb0d1e7e52a62fdfa55ba5883e
Kirill <pnchkirill@gmail.com>
2022-08-06 17:58:18 +0300

    Tests updated

--- src/core/tests/test_utils.py
+++ src/core/tests/test_utils.py
@@ -11,7 +11,7 @@ def test_parser_all_default_args_value():
 
 def test_get_arguments_return_value():
     args = get_arguments('-r 55 -s')
-    assert args['results'] == '55'
-    assert not args['separate']
+    assert args.results == '55'
+    assert not args.separate
     args = get_arguments('-r')
-    assert args['results'].replace(microsecond=0) == datetime.now().replace(microsecond=0)
+    assert args.results.replace(microsecond=0) == datetime.now().replace(microsecond=0)
